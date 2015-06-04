de66d87f085fb6234c2e729a027bb4c5f099e7a2
jamiesun <jamiesun.net@gmail.com>
2015-06-04 10:51:54 +0800

    ui

--- fabfile.py
+++ fabfile.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-import sys,os
-sys.path.insert(0,os.path.dirname(__file__))
+import sys, os
+
+sys.path.insert(0, os.path.dirname(__file__))
 from fabric.api import *
 from toughradius import __version__
 
 env.user = 'root'
-env.hosts = ['128.199.107.135']
+env.hosts = ['121.40.178.134']
 
-def pypi():
-    with cd("/opt/toughradius"):
-        run('git pull origin stable')
-        run('make upload')
 
-def tag():
-    local("git tag -a v%s -m 'version %s'"%(__version__,__version__))
-    local("git push all v%s:v%s"%(__version__,__version__))
+def deploy():
+    with cd("/var/www/toughradius.org"):
+        run('git pull origin master')
+        run('service nginx reload')
+
+
+
+    
